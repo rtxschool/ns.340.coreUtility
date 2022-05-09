f9d315480b6c405f15ec56fc99ba2c8414fd66ea
rtx school <rtxschool@gmail.com>
2022-05-09 09:59:04 -0700

    revised for latitute & longitude

--- app/src/main/java/com/rtxschool/zombies/cam_p.java
+++ app/src/main/java/com/rtxschool/zombies/cam_p.java
@@ -1,7 +1,6 @@
 package com.rtxschool.zombies;
 
-public class cam_p
-{
+public class cam_p {
     public String ID = "";
 
     public String nomencl = "";
@@ -9,5 +8,8 @@ public class cam_p
     public String url = "";
 
     public String type = "";
-}
 
+    public double coor_x = 0;
+
+    public double coor_y = 0;
+}
